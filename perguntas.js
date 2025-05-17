333c5f4b236ce09fc5ee7085e68e19a52c54db06
Marcos <marcos.filho27@escola.pr.gov.br>
2025-05-17 02:16:58 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,19 @@
 criaCartao(
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é Desmatamento?',
+    'Desmatamento, também conhecido como desflorestação, é a remoção total ou parcial da cobertura vegetal, geralmente florestas, para outros usos, como agricultura, pastagem, mineração ou urbanização. É um dos principais problemas ambientais globais, com consequências significativas para o clima, a biodiversidade e os ecossistemas. '
 )
 
 criaCartao(
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Malefícios do Desmatamento',
+    'O desmatamento traz uma série de consequências negativas, afetando o meio ambiente, a sociedade e a economia. Entre os principais malefícios estão a perda de biodiversidade, a alteração do clima, a erosão do solo, a contaminação da água, a perda de serviços ecossistêmicos e a violação dos direitos humanos. '
 )
 
 criaCartao(
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Meios para impedir o desmatamento',
+    'Para combater o desmatamento, são necessárias ações em diversas frentes, desde a implementação de políticas públicas eficientes até a mudança de atitudes individuais. A fiscalização e controle ambiental, a conscientização da população, o desenvolvimento de atividades sustentáveis e o uso de tecnologias de monitoramento são cruciais. '
 )
 
 criaCartao(
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'O que o desmatamento pode causar aos animais?',
+    'O desmatamento tem um impacto devastador nos animais, levando à perda de habitat, redução da biodiversidade, aumento do risco de predação e extinção de espécies. A derrubada de árvores elimina os locais onde os animais vivem, se reproduzem e se alimentam, forçando-os a migrar ou a morrer. A perda de habitat também interrompe a cadeia alimentar, afetando a disponibilidade de alimentos e aumentando a competição entre as espécies. '
 )
