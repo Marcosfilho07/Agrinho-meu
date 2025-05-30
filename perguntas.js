e8d9c0146a85a25c75d0d7e910fdf5c2e020a98c
Marcos <146374998+Marcosfilho07@users.noreply.github.com>
2025-05-30 15:43:23 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Desmatamento',
     'O que é Desmatamento?',
-    'Desmatamento é a retirada total ou parcial da vegetação, principalmente florestas, para usos como agricultura, mineração ou expansão urbana. É um dos maiores problemas ambientais do mundo, com impacto direto no clima e na biodiversidade. '
+    'Desmatamento é a retirada total ou parcial da vegetação, principalmente florestas, para usos como agricultura, mineração ou expansão urbana. É um dos maiores problemas ambientais do mundo, com impacto direto no clima e na biodiversidade.'
 )
 
 criaCartao(
     'Malefícios',
     'Malefícios do Desmatamento!',
-    'O desmatamento causa perda de biodiversidade, mudanças climáticas, erosão do solo, poluição da água e desequilíbrio dos ecossistemas. Também pode afetar comunidades locais e seus direitos. '
+    'O desmatamento causa perda de biodiversidade, mudanças climáticas, erosão do solo, poluição da água e desequilíbrio dos ecossistemas. Também pode afetar comunidades locais e seus direitos.'
 )
 
 criaCartao(
     'Como ajudar',
     'Meios para impedir o desmatamento!',
-    'É preciso agir com políticas públicas, fiscalização, educação ambiental e uso de tecnologias. Atitudes sustentáveis, tanto coletivas quanto individuais, são essenciais para proteger as florestas. '
+    'É preciso agir com políticas públicas, fiscalização, educação ambiental e uso de tecnologias. Atitudes sustentáveis, tanto coletivas quanto individuais, são essenciais para proteger as florestas.'
 )
 
 criaCartao(
     'Causa',
     'O que o desmatamento pode causar aos animais?',
-    'O desmatamento tem um impacto devastador nos animais, levando à perda de habitat, redução da biodiversidade, aumento do risco de predação e extinção de espécies. A derrubada de árvores elimina os locais onde os animais vivem, se reproduzem e se alimentam, forçando-os a migrar ou a morrer. A perda de habitat também interrompe a cadeia alimentar, afetando a disponibilidade de alimentos e aumentando a competição entre as espécies. '
+    'O desmatamento tem um impacto devastador nos animais, levando à perda de habitat, redução da biodiversidade, aumento do risco de predação e extinção de espécies. A derrubada de árvores elimina os locais onde os animais vivem, se reproduzem e se alimentam, forçando-os a migrar ou a morrer.'
 )
