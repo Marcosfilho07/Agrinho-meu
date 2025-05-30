6401baa88335c41dff3e477cdcf3cf5d4ab6f6b3
Marcos <146374998+Marcosfilho07@users.noreply.github.com>
2025-05-30 15:50:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -31,9 +31,3 @@ function criaCartao(categoria, pergunta, resposta) {
 
     container.appendChild(cartao)
 }
-    cartao.addEventListener('click', viraCartao)
-
-
-    container.appendChild(cartao)
-
-}
